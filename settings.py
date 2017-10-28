ad385e5e37b5fd5b1234a3c827e9dd0de6920237
Chris Peake <github@chrispeake.uk>
2017-10-28 11:35:41 +0100

    Added comments to more settings

--- settings.py
+++ settings.py
@@ -3,6 +3,8 @@ __version__ = 0.00
 # NOTE: this setting scales up the size of betting across all strategies (USE WITH CAUTION!!!)
 stake_multiplier = 1.0
 
+# The minimum stake a bet can be placed at on Betfair
 minimum_stake = 2.0
 
+# Increment ladder for scaling up bet stakes
 stake_ladder = [1.0, 1.0, 2.0, 4.0, 8.0, 16.0]
