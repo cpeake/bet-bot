02b8d3d2fbf2aef67343567b000370284b3587a6
Chris Peake <github@chrispeake.uk>
2017-11-02 19:08:36 +0000

    Safer stake and weight ladder increments for testing

--- settings.py
+++ settings.py
@@ -7,7 +7,9 @@ stake_multiplier = 1.0
 minimum_stake = 2.0
 
 # Increment ladder for scaling up bet stakes
-stake_ladder = [1.0, 1.0, 2.0, 4.0, 8.0, 16.0]
+# stake_ladder = [1.0, 1.0, 2.0, 4.0, 8.0, 16.0]
+stake_ladder = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]  # Safer strategy testing!
 
 # Increment ladder for scaling up bet weights
-weight_ladder = [1.0, 1.0, 2.0, 4.0, 8.0, 16.0]
+# weight_ladder = [1.0, 2.0, 4.0, 8.0, 16.0]
+weight_ladder = [1.0, 2.0, 3.0, 4.0, 5.0]  # Safer strategy testing!
