855ea78bf64aacc5862c4dfe5db724ce954fb0f2
Chris Peake <github@chrispeake.uk>
2017-11-16 17:55:23 +0000

    Added total statistics

--- threads/statistics_manager.py
+++ threads/statistics_manager.py
@@ -36,14 +36,36 @@ class StatisticsManager(threading.Thread):
                 mtd_pnls = betbot_db.order_repo.get_mtd_pnls()
                 ytd_pnls = betbot_db.order_repo.get_ytd_pnls()
                 lifetime_pnls = betbot_db.order_repo.get_lifetime_pnls()
+                daily_total = 0.0
+                wtd_total = 0.0
+                mtd_total = 0.0
+                ytd_total = 0.0
+                lifetime_total = 0.0
                 for statistic in statistics:
                     strategy_ref = statistic['strategyRef']
-                    statistic['dailyPnL'] = daily_pnls[strategy_ref] if strategy_ref in daily_pnls else 0
-                    statistic['weeklyPnL'] = wtd_pnls[strategy_ref] if strategy_ref in wtd_pnls else 0
-                    statistic['monthlyPnL'] = mtd_pnls[strategy_ref] if strategy_ref in mtd_pnls else 0
-                    statistic['yearlyPnL'] = ytd_pnls[strategy_ref] if strategy_ref in ytd_pnls else 0
-                    statistic['lifetimePnL'] = lifetime_pnls[strategy_ref] if strategy_ref in lifetime_pnls else 0
+                    daily_pnl = daily_pnls[strategy_ref] if strategy_ref in daily_pnls else 0
+                    wtd_pnl = wtd_pnls[strategy_ref] if strategy_ref in wtd_pnls else 0
+                    mtd_pnl = mtd_pnls[strategy_ref] if strategy_ref in mtd_pnls else 0
+                    ytd_pnl = ytd_pnls[strategy_ref] if strategy_ref in ytd_pnls else 0
+                    lifetime_pnl = lifetime_pnls[strategy_ref] if strategy_ref in lifetime_pnls else 0
+                    daily_total += daily_pnl
+                    wtd_total += wtd_pnl
+                    mtd_total += mtd_pnl
+                    ytd_total += ytd_pnl
+                    lifetime_total += lifetime_pnl
+                    statistic['dailyPnL'] = daily_pnl
+                    statistic['weeklyPnL'] = wtd_pnl
+                    statistic['monthlyPnL'] = mtd_pnl
+                    statistic['yearlyPnL'] = ytd_pnl
+                    statistic['lifetimePnL'] = lifetime_pnl
                     betbot_db.statistic_repo.upsert(statistic)
+                total_statistic = betbot_db.statistic_repo.get_by_reference('TOTALS')
+                total_statistic['dailyPnL'] = daily_total
+                total_statistic['weeklyPnL'] = wtd_total
+                total_statistic['monthlyPnL'] = mtd_total
+                total_statistic['yearlyPnL'] = ytd_total
+                total_statistic['lifetimePnL'] = lifetime_total
+                betbot_db.statistic_repo.upsert(total_statistic)
 
                 now = time()
                 tomorrow1am = helpers.get_tomorrow_start_of_day() + timedelta(hours=1)
