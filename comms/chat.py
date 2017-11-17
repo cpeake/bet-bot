2e81718ddcb12be8f24eb35b8235036079a67521
Chris Peake <github@chrispeake.uk>
2017-11-17 19:02:34 +0000

    Added a chat manager for Slack messages

---
+++ comms/chat.py
@@ -0,0 +1,27 @@
+import os
+import logging
+from slackclient import SlackClient
+
+# Set up logging
+logger = logging.getLogger('CHATM')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('(%(name)s) - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+channel = os.environ["SLACK_CHANNEL"]
+sc = SlackClient(slack_token)
+
+
+class ChatManager(object):
+    @staticmethod
+    def post_message(msg=''):
+        logger.info('Posted message "%s" to Slack channel %s.' % (msg, channel))
+        sc.api_call(
+          "chat.postMessage",
+          channel=channel,
+          text=msg
+        )
