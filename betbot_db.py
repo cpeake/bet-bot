6f2c61a77bb757fc0966fbf14463530ec97fb019
Chris Peake <github@chrispeake.uk>
2017-12-01 18:40:01 +0000

    Fix upsert key generation

--- betbot_db.py
+++ betbot_db.py
@@ -193,8 +193,6 @@ class RunnerBookRepository(object):
 
     def upsert(self, runner_book=None):
         if runner_book:
-            self.logger.info("Upserting runner book: %s" % runner_book)
-            # do datetime conversions first
             key = {
                 'marketId': runner_book['marketId'],
                 'selectionId': runner_book['runners'][0]['selectionId']
