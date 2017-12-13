7737da0a61dc04f79f3038ff6d3d88ee42600c53
Chris Peake <github@chrispeake.uk>
2017-12-13 18:29:58 +0000

    Bug in runner book retrieval

--- threads/order_manager.py
+++ threads/order_manager.py
@@ -107,7 +107,7 @@ class OrderManager(threading.Thread):
                 selection_id = instruction['instruction']['selectionId']
                 side = instruction['instruction']['side']
                 strategy_ref = instruction['strategyRef']
-                outcome = self.get_outcome(market_id, selection_id, side, simulated=True)
+                outcome = self.get_outcome(market_id, selection_id, side, True)
                 self.logger.info(outcome)
                 if not outcome['result'] or outcome['indicative']:
                     order = {
